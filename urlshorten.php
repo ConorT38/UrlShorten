c718d6cc3eade0fed4cd739dd3c1093e8c89ad04
Conor Thompson <conorthompson213@gmail.com>
2015-10-08 16:40:03 +0100

    Create urlshorten.php

---
+++ urlshorten.php
@@ -0,0 +1,17 @@
+<?php
+echo'<form action="urlshorten.php" method="post">'
+    .'<input type="text" name="url" placeholder="Enter in your url here:"/>'
+    .'<input type="submit" name="submit"/>'
+    .'</form>';
+
+if(isset($_POST['submit']))
+{
+$url = $_POST['url'];
+$id= rand(0,100000000);
+
+$f = fopen("$id.php",'w');
+fwrite($f,'<?php header("Location: '.$url.'"); ?>');
+fclose($f);
+}
+echo 'Your shortened url: www.codejackal.com/Kieran/'.$id.'.php';
+?>
